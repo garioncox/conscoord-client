1afbaf7271a86abca6728f7690e0fab99620d443
brycecoon <112597142+brycecoon@users.noreply.github.com>
2024-10-11 11:13:02 -0600

    bryce ticket

--- src/Pages/EmployeeList.tsx
+++ src/Pages/EmployeeList.tsx
@@ -1,4 +1,4 @@
-import { useEffect } from "react";
+import { useEffect, useState } from "react";
 import { useNavigate } from "react-router-dom";
 import "../index.css";
 import AddOfficer from "./AddOfficer";
@@ -6,17 +6,31 @@ import {
   useGetEmployees,
   useGetRoles,
 } from "../Functions/EmployeeListFunctions";
+import Role from "../Data/Interfaces/RoleInterface";
 
 const EmployeeList = () => {
   const { employees, fetchEmployees } = useGetEmployees();
   const { roles, fetchRoles } = useGetRoles();
+  const [selectedRole, setSelectedRole] = useState<number | undefined>(-1);
+  const [selectedEmployee, setSelectedEmployee] = useState<number | null>(-1);
 
   useEffect(() => {
     fetchEmployees();
     fetchRoles();
-  }, [fetchEmployees, fetchRoles]);
+  }, []);
 
   const navigate = useNavigate();
+
+  const updateUserRole = (employeeId: number, currentRoleId: number) => {
+    setSelectedEmployee(employeeId);
+    setSelectedRole(currentRoleId);
+  };
+
+  const saveUserRole = () => {
+    //set the role id here
+    // setSelectedEmployee(null);
+    // setSelectedRole(undefined);  
+  };
 
   const contents =
     employees === undefined ? (
@@ -29,6 +43,7 @@ const EmployeeList = () => {
             <th className="text-start">Phone Number</th>
             <th className="text-start">Email</th>
             <th className="text-start">Role</th>
+            <th className="text-start">Action</th>
           </tr>
         </thead>
         <tbody>
@@ -36,14 +51,54 @@ const EmployeeList = () => {
             <tr
               key={e.id}
               className="grow grow:hover"
-              onClick={() => navigate(`/admin/view/employees/${e.id}`)}
+              // onClick={() => navigate(`/admin/view/employees/${e.id}`)}
             >
               <td className="text-start">{e.name}</td>
               <td className="text-start">{e.phonenumber}</td>
               <td className="text-start">{e.email}</td>
-              <td className="text-start">
-                {roles.find((role) => role.id === e.roleid)?.rolename}
-              </td>
+
+              {selectedEmployee === e.id ? (
+              <>
+                <td className="text-start">
+                  {/* <label htmlFor="selectRole">Select Role</label> */}
+                  <select
+                    className="form-select"
+                    name="selectRole"
+                    onChange={(e) => {
+                         const selectedRoleId = roles.find((role: Role) => role.rolename === e.target.value)?.id;
+                         setSelectedRole(selectedRoleId ?? 1);
+                       }}                  >
+                    <option value="">
+                      Default (No Role)
+                    </option>
+                    {roles.map((role) => (
+                      <option key={role.id} value={role.id}>
+                        {role.rolename}
+                      </option>
+                    ))}
+                  </select>
+                </td>
+                <td>
+                  <button className="btn btn-info" onClick={saveUserRole}>
+                    Save Changes
+                  </button>
+                </td>
+              </>
+            ) : (
+              <>
+                <td className="text-start">
+                  {roles.find((role) => role.id === e.roleid)?.rolename}
+                </td>
+                <td>
+                  <button
+                    className="btn btn-info"
+                    onClick={() => updateUserRole(e.id, e.roleid)}
+                  >
+                    Update Role
+                  </button>
+                </td>
+              </>
+            )}
             </tr>
           ))}
         </tbody>
@@ -54,6 +109,7 @@ const EmployeeList = () => {
     <>
       <AddOfficer />
       <h1>Admin Employee View</h1>
+      <div>Selected Role is {selectedRole}</div>
       {contents}
     </>
   );
