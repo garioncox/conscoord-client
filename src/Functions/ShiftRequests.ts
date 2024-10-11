d180949770cd9c206f7148f8bb416dce11051bcc
brycecoon <112597142+brycecoon@users.noreply.github.com>
2024-10-11 17:43:30 -0600

    linted

--- src/Functions/ShiftRequests.ts
+++ src/Functions/ShiftRequests.ts
@@ -3,33 +3,33 @@ import { Shift } from "../Data/Interfaces/Shift";
 import { ShiftDTO } from "../Data/DTOInterfaces/ShiftDTO";
 
 export const useShiftRequests = () => {
-    const addShift = async (shift: ShiftDTO) => {
-        await axios.post(`/api/Shift/add`, shift);
-      };
-    
-      const editShift = async (id: number, shift: ShiftDTO) => {
-        await axios.post(`/api/Shift/edit/${id}`, shift);
-      };
-    
-      const getAllShifts = async (): Promise<Shift[]> => {
-        const response = await axios.get(`/api/Shift/getAll`);
-        return response.data;
-      };
+  const addShift = async (shift: ShiftDTO) => {
+    await axios.post(`/api/Shift/add`, shift);
+  };
 
-      const getAllArchivedShifts = async (): Promise<Shift[]> => {
-        const response = await axios.get(`/api/Shift/getAll/archived`);
-        return response.data;
-      };
-    
-      const archiveShift = async (shiftId: number) => {
-        await axios.post(`/api/Shift/archive/${shiftId}`);
-      };
+  const editShift = async (id: number, shift: ShiftDTO) => {
+    await axios.post(`/api/Shift/edit/${id}`, shift);
+  };
 
-      return {
-        addShift,
-        editShift,
-        getAllShifts,
-        getAllArchivedShifts,
-        archiveShift,
-      }
-}
+  const getAllShifts = async (): Promise<Shift[]> => {
+    const response = await axios.get(`/api/Shift/getAll`);
+    return response.data;
+  };
+
+  const getAllArchivedShifts = async (): Promise<Shift[]> => {
+    const response = await axios.get(`/api/Shift/getAll/archived`);
+    return response.data;
+  };
+
+  const archiveShift = async (shiftId: number) => {
+    await axios.post(`/api/Shift/archive/${shiftId}`);
+  };
+
+  return {
+    addShift,
+    editShift,
+    getAllShifts,
+    getAllArchivedShifts,
+    archiveShift,
+  };
+};
