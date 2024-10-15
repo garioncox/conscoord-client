316a4f77f64a4734b9b68ac71c12e9767e81d027
kalin.nebeker <kalin.nebeker@ENGR9HCP5S3>
2024-10-15 15:18:32 -0600

    resign from shifts

--- src/Pages/ShiftOfficerList.tsx
+++ src/Pages/ShiftOfficerList.tsx
@@ -9,74 +9,120 @@ import { toast, ToastContainer } from "react-toastify";
 import "react-toastify/dist/ReactToastify.css";
 import { EmailRequest } from "../Data/Interfaces/Email";
 import PermissionLock, { PSO_ROLE } from "../Components/PermissionLock";
+import { useEmployeeRequests } from "../Functions/EmployeeRequests";
 
 function ShiftOfficerList() {
-  const {addEmployeeShift} = useEmpShiftRequests();
-  const {getAllShifts} = useShiftRequests();
-  const {sendEmail} = useEmailRequests();
+  const { addEmployeeShift, getSignedUpShifts, deleteEmployeeShift } = useEmpShiftRequests();
+  const { getEmployeeByEmail } = useEmployeeRequests();
+  const { getAllShifts } = useShiftRequests();
+  const { sendEmail } = useEmailRequests();
   const { user } = useAuth0();
 
-  const [shifts, setShifts] = useState<Shift[]>();
+  const [shifts, setShifts] = useState<Shift[]>([]);
+  const [claimedShifts, setClaimedShifts] = useState<Shift[]>([]);
 
   useEffect(() => {
     populateShifts();
-  }, []);
+  }, [user?.email]);
 
   const contents =
     shifts === undefined ? (
       <div className="spinner-border" role="status" />
     ) : (
-      <table className="table table-striped">
-        <thead>
-          <tr>
-            <th>Location</th>
-            <th>Start Time</th>
-            <th>End Time</th>
-          </tr>
-        </thead>
-        <tbody>
-          {shifts.map((s) => (
-            <tr key={s.id}>
-              <td>{s.location}</td>
-              <td>{s.startTime}</td>
-              <td>{s.endTime}</td>
-              <td>
-                <button
-                  className="btn btn-primary"
-                  onClick={() => {
-                    toast.success("Shift Signed Up Successfully");
-                    postEmployeeShift(s.id);
-
-                    if (user && user.email) {
-                      const email: EmailRequest = {
-                        Email: user.email,
-                        Subject: "Shift signup notification",
-                        MessageBody: ` ${user?.name}, you have signed up to the shift at ${s.location} from ${s.startTime} to ${s.endTime}`,
-                      };
-                      sendEmail(email);
-                    }
-                  }}
-                >
-                  Take This Shift
-                </button>
-              </td>
+      <>
+        <table className="table table-striped">
+          <thead>
+            <tr>
+              <th>Location</th>
+              <th>Start Time</th>
+              <th>End Time</th>
+              <th></th>
             </tr>
-          ))}
-        </tbody>
-      </table>
+          </thead>
+          <tbody>
+            {claimedShifts?.map((s) => (
+              <tr key={s.id}>
+                <td>{s.location}</td>
+                <td>{s.startTime}</td>
+                <td>{s.endTime}</td>
+                <td>
+                  <button
+                    className="btn btn-danger"
+                    onClick={() => {
+                      deleteEmployeeShift(s.id)
+                      setClaimedShifts(claimedShifts.filter(shift => shift.id !== s.id))
+                    }
+                    }
+                  >
+                    Resign from shift
+                  </button>
+                </td>
+              </tr>
+            ))}
+          </tbody>
+        </table>
+        <hr />
+
+        <table className="table table-striped">
+          <thead>
+            <tr>
+              <th>Location</th>
+              <th>Start Time</th>
+              <th>End Time</th>
+            </tr>
+          </thead>
+          <tbody>
+            {shifts.map((s) => (
+              <tr key={s.id}>
+                <td>{s.location}</td>
+                <td>{s.startTime}</td>
+                <td>{s.endTime}</td>
+                <td>
+                  <button
+                    className="btn btn-primary"
+                    onClick={() => {
+                      toast.success("Shift Signed Up Successfully"); // TODO: change to reflect status
+                      postEmployeeShift(s.id);
+                      setClaimedShifts((prevClaimedShifts) => [...prevClaimedShifts, s]);
+                      setShifts(shifts?.filter(shift => shift.id !== s.id))
+
+                      if (user && user.email) {
+                        const email: EmailRequest = {
+                          Email: user.email,
+                          Subject: "Shift signup notification",
+                          MessageBody: ` ${user?.name}, you have signed up to the shift at ${s.location} from ${s.startTime} to ${s.endTime}`,
+                        };
+                        sendEmail(email);
+                      }
+                    }}
+                  >
+                    Take This Shift
+                  </button>
+                </td>
+              </tr>
+            ))}
+          </tbody>
+        </table>
+      </>
     );
 
   async function populateShifts() {
+    if (user && user.email !== undefined) {
+      setClaimedShifts(await getSignedUpShifts(user.email));
+    }
     setShifts(await getAllShifts());
   }
 
   async function postEmployeeShift(id: number) {
-    const employee: EmployeeShiftDTO = {
-      EmployeeId: 1,
-      ShiftId: id,
-    };
+    if (user && user.email) {
+      const currUser = await getEmployeeByEmail(user.email);
+      const employee: EmployeeShiftDTO = {
+        EmployeeId: currUser.id,
+        ShiftId: id,
+      };
 
-    await addEmployeeShift(employee);
+      await addEmployeeShift(employee);
+    }
   }
 
   return (
