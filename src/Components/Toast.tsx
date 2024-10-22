e6d8292414e5d8d36795bb47a6a6d7f4fed3ede9
bryce.coon <bryce.coon@ENGRBHCP5S3>
2024-10-21 18:03:00 -0600

    [KGB-135] Toasts return response body

--- src/Components/Toast.tsx
+++ src/Components/Toast.tsx
@@ -2,27 +2,28 @@ import { toast } from "react-toastify";
 import "react-toastify/dist/ReactToastify.css";
 
 export const useCustomToast = () => {
-    const createToast = (
-        asyncFunction: any,
-        params: any,
-        defaultMessage: string
-    ) => {
-        toast.promise(asyncFunction(params), {
-            pending: `${defaultMessage}`,
-            success: {
-                render() {
-                    return `${defaultMessage} Successful!`;
-                },
-            },
-            error: {
-                render({ data }: { data: any }) {
-                    return data.message || `Error ${defaultMessage}`;
-                },
-            },
-        });
-    };
+  const createToast = async (
+    asyncFunction: any,
+    params: any,
+    defaultMessage: string
+  ) => {
+    const response: any = await toast.promise(asyncFunction(params), {
+      pending: `${defaultMessage}`,
+      success: {
+        render() {
+          return `${defaultMessage} Successful!`;
+        },
+      },
+      error: {
+        render({ data }: { data: any }) {
+          return data.message || `Error ${defaultMessage}`;
+        },
+      },
+    });
+    return response;
+  };
 
-    return {
-        createToast,
-    };
+  return {
+    createToast,
+  };
 };
