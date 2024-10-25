8ddfba9e0d8d21739755af78a8573d3d3ca4bda5
Garion Cox <garion.cox@students.snow.edu>
2024-10-24 18:11:04 -0600

    Custom Error Page

---
+++ src/Components/Error.tsx
@@ -0,0 +1,11 @@
+export const Error = () => {
+  return (
+    <div>
+      <h1 className="text-danger">
+        Something went wrong. Try refreshing the page to continue.
+      </h1>
+    </div>
+  );
+};
+
+export default Error;
