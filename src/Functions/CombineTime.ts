a464308febe8bc9939d225298275839b32e1b6a3
kalin.nebeker <kalin.nebeker@ENGR9HCP5S3>
2024-11-13 16:46:24 -0700

    removed seconds from time

--- src/Functions/CombineTime.ts
+++ src/Functions/CombineTime.ts
@@ -3,5 +3,8 @@ export function CombineTime(startTime: string, endTime: string) {
     const _startTime = startTime.split(" ")[1];
     const _endTime = endTime.split(" ")[1];
 
-    return `${_startDate} ${_startTime} - ${_endTime}`;
+    const _startTimeNoSeconds = _startTime.slice(0, -3);
+    const _endTimeNoSeconds = _endTime.slice(0, -3);
+
+    return `${_startDate} ${_startTimeNoSeconds} - ${_endTimeNoSeconds}`;
 }
