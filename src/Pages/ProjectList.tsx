99225a066ee1a8468900e99fb5cf8e58a5644ed2
bryce.coon <bryce.coon@ENGRCJCP5S3>
2024-11-14 17:30:30 -0700

    employees can see all projects now

--- src/Pages/ProjectList.tsx
+++ src/Pages/ProjectList.tsx
@@ -3,21 +3,19 @@ import { PaginatedTable } from "@/Components/paginated-table";
 import { usePaginatedTable } from "@/Components/PaginatedTableHook";
 import { Spinner } from "@/Components/Spinner";
 import { Project } from "@/Data/Interfaces/Project";
-import { useAllProjectByLoggedInCompany } from "@/Functions/ProjectRequests";
+import { useAllProjects } from "@/Functions/ProjectRequests";
 import React, { useEffect } from "react";
 import { useNavigate } from "react-router-dom";
 
 function ProjectList() {
-  const { data } = useAllProjectByLoggedInCompany();
+  const { data } = useAllProjects();
   const navigate = useNavigate();
   const [filteredData, setFilteredData] = React.useState<Project[]>([]);  
   const [archived, setArchived] = React.useState(true);
 
-  // Initialize paginated table hook with either archived or filtered data based on checkbox
   const control = usePaginatedTable(archived ? filteredData : data ?? []);
 
   useEffect(() => {
-    // Whenever `data` changes, filter out archived projects for `filteredData`
     setFilteredData(data?.filter((project) => project.status !== "ARCHIVED") || []);
   }, [data]);
 
