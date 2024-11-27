d2181b766c3626c76481eed3d56b7d76018981e4
kalin <kalin.nebeker@yahoo.com>
2024-11-26 18:32:03 -0700

    90% of the way with contact info

--- src/Components/AddProject.tsx
+++ src/Components/AddProject.tsx
@@ -7,11 +7,26 @@ import { useGDateInput } from "./Generics/gDateInputController";
 import { ProjectDTO } from "@/Data/DTOInterfaces/ProjectDTO";
 import { FormatDate } from "@/Functions/FormatDates";
 import { useAddProjectMutation } from "@/Functions/Queries/ProjectQueries";
+import { useState } from "react";
+import { useAddEmployeeMutation, useGetEmployeeByEmail, useLoggedInEmployee } from "@/Functions/Queries/EmployeeQueries";
+import Modal from "./Modal";
+import { useGEmailInput, validateEmail } from "./Generics/gEmailInputController";
+import { useGPhoneInput, validatePhone } from "./Generics/gPhoneInputController";
+import {  } from "@/Functions/EmployeeRequests";
 
 export function AddProject() {
+  const [selfAsContact, setSelfAsContact] = useState<boolean>(true);
+  const { data: loggedInEmployee } = useLoggedInEmployee();
+
+  const [isModalOpen, setIsModalOpen] = useState(false);
+  const toggleModal = () => setIsModalOpen(!isModalOpen);
+
+  const addEmployeeMutation = useAddEmployeeMutation();
+
   const location = useGTextInput("", (v) =>
     v.length === 0 ? "Pleae add a location" : ""
   );
+
   const startDate = useGDateInput("", (s: string) => {
     if (s === "") {
       return "Start date is required";
@@ -25,6 +40,7 @@ export function AddProject() {
 
     return "";
   });
+
   const endDate = useGDateInput("", (s: string) => {
     if (s === "") {
       return "End date is required";
@@ -41,55 +57,128 @@ export function AddProject() {
 
     return "";
   });
+
   const description = useGTextInput("", (v) =>
     v.length === 0 ? "Please add a name" : ""
   );
 
-  const addProject = useAddProjectMutation();
+  const name = useGTextInput("", (v) =>
+    v.length === 0 ? "Please add a name" : ""
+  );
 
-  function AddProject() {
+  const email = useGEmailInput("", validateEmail);
+  const phonenumber = useGPhoneInput("", validatePhone);
+
+  const addProjectMutation = useAddProjectMutation();
+  const { refetch: fetchEmployee, data: newEmp } = useGetEmployeeByEmail(email.value);
+
+  function ProjectControl(id:number) {
+    if (selfAsContact) {
+      AddProject(id);
+    } else {
+      toggleModal();
+      return;
+    }
+  }
+
+  function AddProject(id: number) {
+    if (!loggedInEmployee) { console.log("No employee found"); return; }
     const project: ProjectDTO = {
       name: description.value,
       location: location.value,
       startDate: FormatDate(startDate.value),
       endDate: FormatDate(endDate.value),
+      contactinfo:id,
     };
-    addProject.mutate({ project });
+    addProjectMutation.mutate({ project });
+  }
 
-    location.setValue("");
-    description.setValue("");
-    startDate.setValue("");
-    endDate.setValue("");
+  function AddEmployee() {
+    addEmployeeMutation.mutate({
+      name: name.value,
+      email: email.value,
+      phonenumber: phonenumber.value,
+    });
   }
 
   return (
-    <TableRow>
-      <TableCell>
-        <div>
-          <GTextInput control={description} />
-        </div>
-      </TableCell>
-      <TableCell>
+    <>
+      <TableRow>
+        <TableCell>
+          <div>
+            <GTextInput control={description} />
+          </div>
+        </TableCell>
+        <TableCell>
+          <div className="">
+            <GTextInput control={location} />
+          </div>
+        </TableCell>
+        <TableCell>
+          <div>
+            <GDateInput control={startDate} />
+          </div>
+        </TableCell>
+        <TableCell>
+          <div>
+            <GDateInput control={endDate} />
+          </div>
+        </TableCell>
+
+        <TableCell className="p-4">
+          <label htmlFor="remember">
+            Your contact info listed?
+            <div>
+              <input type="checkbox" className="checkbox" onChange={() => setSelfAsContact(!selfAsContact)} checked={selfAsContact} name="YourContact" id="Contact" />
+            </div>
+          </label>
+        </TableCell>
+        <TableCell>
+          <div onClick={() => ProjectControl(loggedInEmployee?.id ? loggedInEmployee?.id : -1)} className="text-primary hover:text-secondary">
+            <Save />
+          </div>
+        </TableCell>
+      </TableRow>
+
+      <Modal isOpen={isModalOpen} onClose={toggleModal}>
         <div className="">
-          <GTextInput control={location} />
+          <div>
+            <h1>Add contact info</h1>
+            <label>Name
+              <GTextInput control={name} />
+            </label>
+            <label>Email
+              <GTextInput control={email} />
+            </label>
+            <label>Phone Number
+              <GTextInput control={phonenumber} />
+            </label>
+          </div>
+          <div className="flex grow flex-row mt-5">
+            <button
+              onClick={toggleModal}
+              className="ms-auto me-3 p-2 px-4 bg-slate-400 text-white rounded hover:bg-slate-500"
+            >
+              Close
+            </button>
+            <button
+              onClick={async () => {
+                AddEmployee();
+                await fetchEmployee(); 
+                if (newEmp)
+                  await AddProject(newEmp.id);
+                email.setValue("");
+                phonenumber.setValue("");
+                name.setValue("");
+                toggleModal();
+              }}
+              className="p-2 px-4 bg-green-500 hover:bg-green-600 text-black rounded"
+            >
+              Add Contact Info
+            </button>
+          </div>
         </div>
-      </TableCell>
-      <TableCell>
-        <div>
-          <GDateInput control={startDate} />
-        </div>
-      </TableCell>
-      <TableCell>
-        <div>
-          <GDateInput control={endDate} />
-        </div>
-      </TableCell>
-      <TableCell>ACTIVE</TableCell>
-      <TableCell>
-        <div onClick={AddProject} className="text-primary hover:text-secondary">
-          <Save />
-        </div>
-      </TableCell>
-    </TableRow>
+      </Modal>
+    </>
   );
 }
--- src/Functions/Queries/EmployeeQueries.ts
+++ src/Functions/Queries/EmployeeQueries.ts
@@ -1,5 +1,5 @@
-import { useQuery } from "@tanstack/react-query";
-import { getAllEmployees, getEmployeeByEmail } from "../EmployeeRequests";
+import { useMutation, useQuery } from "@tanstack/react-query";
+import { getAllEmployees, getEmployeeByEmail, useEmployeeRequests } from "../EmployeeRequests";
 import { useAuth0 } from "@auth0/auth0-react";
 import { queryKeys } from "./QueryKeyFactory";
 import { Employee } from "@/Data/Interfaces/EmployeeInterface";
@@ -15,6 +15,22 @@ export const useLoggedInEmployee = () => {
       return getEmployeeByEmail(user!.email!);
     },
     enabled: !!(isAuthenticated && user),
+  });
+};
+
+export const useAddEmployeeMutation = () => {
+  const { addEmployee } = useEmployeeRequests();
+  return useMutation({
+    mutationFn: addEmployee
+  });
+};
+
+export const useGetEmployeeByEmail = (email: string) => {
+  const { getEmployeeByEmail } = useEmployeeRequests();
+  return useQuery({
+    queryKey: queryKeys.employeeByEmail,
+    queryFn: () => getEmployeeByEmail(email),
+    enabled: !!email,
   });
 };
 
