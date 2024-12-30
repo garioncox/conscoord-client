92334db717f1aa64c30bbb4b01e6e4596bf2953b
Garion Cox <garion.cox@students.snow.edu>
2024-12-29 17:00:33 -0700

    Added 'Complete' filtering; Extracted ProjectList control into component

--- src/Pages/ProjectList.tsx
+++ src/Pages/ProjectList.tsx
@@ -1,65 +1,24 @@
 import { usePagination } from "@/Components/PaginatedTableHook";
 import { ProjectTable } from "@/Components/Tables/ProjectTable";
 import { Spinner } from "@/Components/Spinner";
-import { Project } from "@/Data/Interfaces/Project";
-import React, { useEffect, useState } from "react";
 import { useNavigate } from "react-router-dom";
-import { CLIENT_ROLE } from "@/Components/Auth/PermissionLock";
 import { Checkbox, FormControl, MenuItem, Select } from "@mui/material";
-import { useRoleQuery } from "@/Functions/RoleProvider";
 import { useProjectSort } from "@/Components/Sorting/ProjectSort";
-import {
-  useAllProjects,
-  useAllProjectByLoggedInCompany,
-} from "@/Functions/Queries/ProjectQueries";
 import { ChevronLeft, ChevronRight } from "lucide-react";
 import { Button } from "@/Components/ui/button";
+import { useProjectListControl } from "@/Pages/Control/ProjectListControl";
 
 function ProjectList() {
-  const { data, isLoading } = useAllProjects();
-  const { data: clientProjects } = useAllProjectByLoggedInCompany();
-
-  const roleQuery = useRoleQuery();
   const navigate = useNavigate();
 
-  const [filteredData, setFilteredData] = React.useState<Project[]>([]);
-  const [archived, setArchived] = React.useState(true);
-  const [sortedData, setSortedData] = useState<Project[] | null>([]);
+  const control = useProjectListControl();
+  const paginationControl = usePagination(control.sortedData || []);
+  const sortControl = useProjectSort(
+    control.sortedData ?? [],
+    control.setSortedData
+  );
 
-  const paginationControl = usePagination(sortedData || []);
-  const sortControl = useProjectSort(sortedData ?? [], setSortedData);
-
-  useEffect(() => {
-    if (roleQuery && roleQuery.data === CLIENT_ROLE && clientProjects) {
-      const defaultSort = [...clientProjects].sort(
-        (a, b) =>
-          new Date(a.startDate).getTime() - new Date(b.startDate).getTime()
-      );
-      setSortedData(defaultSort);
-    } else if (data) {
-      if (archived) {
-        const defaultSort = [...filteredData].sort(
-          (a, b) =>
-            new Date(a.startDate).getTime() - new Date(b.startDate).getTime()
-        );
-        setSortedData(defaultSort);
-      } else {
-        const defaultSort = [...data].sort(
-          (a, b) =>
-            new Date(a.startDate).getTime() - new Date(b.startDate).getTime()
-        );
-        setSortedData(defaultSort);
-      }
-    }
-  }, [data, filteredData, archived, clientProjects]);
-
-  useEffect(() => {
-    setFilteredData(
-      data?.filter((project) => project.status !== "ARCHIVED") || []
-    );
-  }, [data]);
-
-  if (isLoading) {
+  if (control.isLoading) {
     return <Spinner />;
   }
 
@@ -86,7 +45,6 @@ function ProjectList() {
               },
             }}
           >
-            <MenuItem value="">Select an option</MenuItem>
             <MenuItem value="Name">Name</MenuItem>
             <MenuItem value="Location">Location</MenuItem>
             <MenuItem value="startDateAsc">Soonest First</MenuItem>
@@ -95,19 +53,37 @@ function ProjectList() {
             <MenuItem value="endDateDesc">Latest End Date First</MenuItem>
           </Select>
         </FormControl>
-        <div className="h-[1px] w-8 bg-gray-300 hidden md:block mx-3"></div>
+
+        <div className="h-[1px] w-8 bg-gray-300 hidden md:block mx-3" />
+
         <div className="pe-2 flex items-center">
           <div className="me-2">Archived</div>
           <Checkbox
-            checked={!archived}
+            checked={control.archivedSelected}
             onChange={() => {
-              setArchived(!archived);
+              control.setIsArchivedSelected(!control.archivedSelected);
               paginationControl.setCurrentPage(1);
             }}
             className="ms-1 h-3 w-3"
           />
         </div>
-        <div className="h-[1px] bg-gray-300 flex-grow hidden md:block mx-3"></div>{" "}
+
+        <div className="h-[1px] w-8 me-2 bg-gray-300 hidden md:block" />
+
+        <div className="pe-2 flex items-center">
+          <div className="me-2">Complete</div>
+          <Checkbox
+            checked={control.completedSelected}
+            onChange={() => {
+              control.setIsCompletedSelected(!control.completedSelected);
+              paginationControl.setCurrentPage(1);
+            }}
+            className="ms-1 h-3 w-3"
+          />
+        </div>
+
+        <div className="h-[1px] bg-gray-300 flex-grow hidden md:block mx-3" />
+
         <div>Items per page</div>
         <FormControl sx={{ mx: 1, minWidth: 55 }} size="small">
           <Select
@@ -129,7 +105,9 @@ function ProjectList() {
             <MenuItem value={20}>20</MenuItem>
           </Select>
         </FormControl>
+
         <div className="h-[1px] w-8 bg-gray-300 hidden md:block" />
+
         <div className="flex items-center space-x-2 ms-2">
           <Button
             variant="outline"
