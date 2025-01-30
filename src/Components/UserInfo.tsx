03174c12a02edcc0e6f0e3c2e3d5c3c40cc30dc7
Garion Cox <garion.cox@students.snow.edu>
2025-01-30 10:26:03 -0700

    User info frontend refactor

--- src/Components/UserInfo.tsx
+++ src/Components/UserInfo.tsx
@@ -5,9 +5,10 @@ import {
 } from "@/Functions/Queries/EmployeeQueries";
 import { useAllEmployeeShifts } from "@/Functions/Queries/EmployeeShiftQueries";
 import { useAllShifts } from "@/Functions/Queries/ShiftQueries";
-import { Autocomplete, TextField } from "@mui/material";
-import { ArrowBigRight, Edit } from "lucide-react";
+import { TextField } from "@mui/material";
+import { ArrowBigRight, Save } from "lucide-react";
 import { useState } from "react";
+import { Spinner } from "./Spinner";
 
 export const UserInfo = () => {
   const editEmployeeMutation = useEditEmployeeMutation();
@@ -23,14 +24,16 @@ export const UserInfo = () => {
   const [EmployeePhoneNumber, setEmployeePhoneNumber] = useState("");
   const [EmployeeRoleId, setEmployeeRoleId] = useState(0);
   const [EmployeeCompanyId, setEmployeeCompanyId] = useState(0);
-  const [EmployeeView, setEmployeeView] = useState(false);
+  const [filterString, setFilterString] = useState("");
 
   const [selection, setSelection] = useState<"info" | "history" | "none">(
     "none"
   );
 
   function EditEmployee() {
-    if (Employee === undefined) return;
+    if (!Employee) return;
+    if (!EditedEmployee()) return;
+
     const employee = {
       id: Employee.id,
       name: EmployeeName,
@@ -41,18 +44,29 @@ export const UserInfo = () => {
     };
     editEmployeeMutation.mutate(employee);
     setEditing(false);
-    setEmployeeName(Employee.name);
-    setEmployeeEmail(Employee.email);
-    setEmployeePhoneNumber(Employee.phonenumber);
-    setEmployeeRoleId(Employee.roleid);
-    setEmployeeCompanyId(Employee.companyid);
+  }
+
+  function EditedEmployee() {
+    if (!Employee) {
+      return false;
+    }
+
+    if (
+      EmployeeName == Employee.name &&
+      EmployeeEmail == Employee.email &&
+      EmployeePhoneNumber == Employee.phonenumber &&
+      EmployeeRoleId == Employee.roleid &&
+      EmployeeCompanyId == Employee.companyid
+    ) {
+      return false;
+    }
+
+    return true;
   }
 
   const handleEmployeeSelect = (selectedEmployee: Employee | null) => {
     if (selectedEmployee) {
       setEmployee(selectedEmployee);
-      setEmployeeView(true);
-      // Directly set the form state based on the selected employee
       setEmployeeName(selectedEmployee.name);
       setEmployeeEmail(selectedEmployee.email);
       setEmployeePhoneNumber(selectedEmployee.phonenumber);
@@ -61,183 +75,49 @@ export const UserInfo = () => {
     }
   };
 
-  if (employeesLoading) {
-    return <div>Loading Users</div>;
+  if (employeesLoading || isEmpShiftsLoading || isShiftsLoading) {
+    return <Spinner />;
   }
 
-  const list = (
-    <div>
-      <Autocomplete
-        disablePortal
-        options={Employees!}
-        getOptionLabel={(option) => option.name || String(option.id)}
-        sx={{ width: 300 }}
-        value={Employee}
-        onChange={(_, selectedEmployee) =>
-          handleEmployeeSelect(selectedEmployee)
-        }
-        renderInput={(params) => <TextField {...params} label="Employees" />}
-        renderOption={(props, option) => (
-          <li
-            {...props}
-            onClick={() => {
-              setEmployee(option);
-              setEmployeeView(true);
-            }}
-          >
-            {option.id}: {option.name}
-          </li>
-        )}
-      />
-    </div>
-  );
-
-  const content =
-    Employee !== undefined && EmployeeView ? (
-      <div className="max-w-4xl mx-auto p-6">
-        <div className="bg-white shadow-lg rounded-lg p-6 space-y-6">
-          {/* Editable Form Section */}
-          <div className="space-y-4">
-            {Editing ? (
-              // Dynamic input fields (avoiding repetition)
-              [
-                { label: "Name", value: EmployeeName, setter: setEmployeeName },
-                {
-                  label: "Email",
-                  value: EmployeeEmail,
-                  setter: setEmployeeEmail,
-                },
-                {
-                  label: "Phone Number",
-                  value: EmployeePhoneNumber,
-                  setter: setEmployeePhoneNumber,
-                },
-                {
-                  label: "Role ID",
-                  value: EmployeeRoleId,
-                  //disable because of e:any
-                  //eslint-disable-next-line
-                  setter: (e: any) =>
-                    setEmployeeRoleId(parseInt(e.target.value)),
-                },
-                {
-                  label: "Company ID",
-                  value: EmployeeCompanyId,
-                  //disable because of e:any
-                  //eslint-disable-next-line
-                  setter: (e: any) =>
-                    setEmployeeCompanyId(parseInt(e.target.value)),
-                },
-              ].map((field, index) => (
-                <div key={index}>
-                  <label className="block text-sm font-medium text-gray-900 mb-2">
-                    {field.label}
-                  </label>
-                  <input
-                    className="w-full p-3 border border-gray-300 rounded-md focus:ring-2 focus:ring-blue-500"
-                    value={field.value}
-                    onChange={(e) => field.setter(e.target.value)}
-                    type={field.label.includes("ID") ? "number" : "text"}
-                  />
-                </div>
-              ))
-            ) : (
-              // Non-editable view of employee info
-              <>
-                <p>{Employee.name}</p>
-                <p>{Employee.email}</p>
-                <p>{Employee.phonenumber}</p>
-                <p>
-                  <strong>Role ID:</strong> {Employee.roleid}
-                </p>
-                <p>
-                  <strong>Company ID:</strong> {Employee.companyid}
-                </p>
-              </>
-            )}
-          </div>
-
-          {/* Buttons Section */}
-          <div className="flex justify-between items-center mt-6">
-            {/* Edit Button */}
-            {!Editing && (
-              <button
-                className="flex items-center text-blue-500 hover:text-blue-700"
-                onClick={() => {
-                  setEditing(true);
-                  setEmployeeName(Employee.name);
-                  setEmployeeEmail(Employee.email);
-                  setEmployeePhoneNumber(Employee.phonenumber);
-                  setEmployeeRoleId(Employee.roleid);
-                  setEmployeeCompanyId(Employee.companyid);
-                }}
-              >
-                <Edit className="mr-2" />
-                Edit{" "}
-              </button>
-            )}
-
-            {/* Back & Save Buttons */}
-            <div className="flex gap-4">
-              <button
-                className="bg-gray-500 hover:bg-gray-700 text-white font-bold py-2 px-6 ml-5 rounded-md"
-                onClick={() => {
-                  setEmployeeView(false);
-                  setEditing(false);
-                }}
-              >
-                Back
-              </button>
-              <button
-                className="bg-green-500 hover:bg-green-700 text-white font-bold py-2 px-6 rounded-md"
-                onClick={() => {
-                  EditEmployee();
-                }}
-              >
-                Save
-              </button>
-            </div>
-          </div>
-        </div>
-      </div>
-    ) : null;
-
-  // return (
-  //   <div className="grid grid-cols-2 gap-4">
-  //     <div className="flex">{list}</div>
-  //     <div className="flex">{content}</div>
-  //   </div>
-  // );
-
-  //////////////////////////////////////////////////////////////////////
   return (
     <div className="flex flex-row grow justify-center align-middle p-12">
       {/* Filter Emp */}
       <div className="flex flex-col w-full max-w-96 rounded border-2 border-slate-300 shadow-md shadow-slate-400">
         <div className="p-4 flex flex-row items-center sticky top-0 bg-slate-200 z-10">
-          <TextField label="Filter" variant="standard" fullWidth />
+          <TextField
+            label="Filter"
+            variant="standard"
+            fullWidth
+            onChange={(e) => setFilterString(e.target.value.toLowerCase())}
+          />
         </div>
 
         <div className="flex flex-col grow pb-4 overflow-x-scroll ">
           {Employees?.sort((a, b) => a.id - b.id).map((e) => {
-            return (
-              <div
-                className={`grid grid-cols-4 gap-0 p-5 border-b ${
-                  Employee != null && Employee.id == e.id
-                    ? "shadow-inner shadow-slate-500 bg-slate-200"
-                    : "cursor-pointer"
-                }`}
-                onClick={() => {
-                  setEmployee(e);
-                  if (selection == "none") {
-                    setSelection("info");
-                  }
-                }}
-              >
-                <p className="col-span-1">{e.id}</p>
-                <p className="col-span-3 truncate">{e.name}</p>
-              </div>
-            );
+            if (
+              String(e.id).includes(filterString) ||
+              e.name.toLowerCase().includes(filterString)
+            ) {
+              return (
+                <div
+                  key={e.id}
+                  className={`grid grid-cols-4 gap-0 p-5 border-b ${
+                    Employee != null && Employee.id == e.id
+                      ? "shadow-inner shadow-slate-500 bg-slate-200"
+                      : "cursor-pointer"
+                  }`}
+                  onClick={() => {
+                    handleEmployeeSelect(e);
+                    if (selection == "none") {
+                      setSelection("info");
+                    }
+                  }}
+                >
+                  <p className="col-span-1">{e.id}</p>
+                  <p className="col-span-3 truncate">{e.name}</p>
+                </div>
+              );
+            }
           })}
         </div>
       </div>
@@ -294,59 +174,63 @@ export const UserInfo = () => {
             })}
 
           {selection == "info" && (
-            <div className="mx-52">
-              {[
-                { label: "Name", value: EmployeeName, setter: setEmployeeName },
-                {
-                  label: "Email",
-                  value: EmployeeEmail,
-                  setter: setEmployeeEmail,
-                },
-                {
-                  label: "Phone Number",
-                  value: EmployeePhoneNumber,
-                  setter: setEmployeePhoneNumber,
-                },
-                {
-                  label: "Role ID",
-                  value: EmployeeRoleId,
-                  //disable because of e:any
-                  //eslint-disable-next-line
-                  setter: (e: any) =>
-                    setEmployeeRoleId(parseInt(e.target.value)),
-                },
-                {
-                  label: "Company ID",
-                  value: EmployeeCompanyId,
-                  //disable because of e:any
-                  //eslint-disable-next-line
-                  setter: (e: any) =>
-                    setEmployeeCompanyId(parseInt(e.target.value)),
-                },
-              ].map((field, index) => (
-                <div key={index} className="my-10">
-                  <label className="block text-sm font-medium text-gray-900 mb-2">
-                    {field.label}
-                  </label>
-                  <input
-                    className="w-full p-3 border border-gray-300 rounded-md focus:ring-2 focus:ring-blue-500"
-                    value={field.value}
-                    onChange={(e) => field.setter(e.target.value)}
-                    type={field.label.includes("ID") ? "number" : "text"}
-                  />
-                </div>
-              ))}
+            <div>
+              <Save
+                className="text-slate-500 hover:text-slate-700 ms-auto me-8 mt-8"
+                onClick={() => EditEmployee()}
+                size={32}
+              />
+              <div className="mx-52">
+                {[
+                  {
+                    label: "Name",
+                    value: EmployeeName,
+                    setter: setEmployeeName,
+                  },
+                  {
+                    label: "Email",
+                    value: EmployeeEmail,
+                    setter: setEmployeeEmail,
+                  },
+                  {
+                    label: "Phone Number",
+                    value: EmployeePhoneNumber,
+                    setter: setEmployeePhoneNumber,
+                  },
+                  {
+                    label: "Role ID",
+                    value: EmployeeRoleId,
+                    //disable because of e:any
+                    //eslint-disable-next-line
+                    setter: (e: any) =>
+                      setEmployeeRoleId(parseInt(e.target.value)),
+                  },
+                  {
+                    label: "Company ID",
+                    value: EmployeeCompanyId,
+                    //disable because of e:any
+                    //eslint-disable-next-line
+                    setter: (e: any) =>
+                      setEmployeeCompanyId(parseInt(e.target.value)),
+                  },
+                ].map((field, index) => (
+                  <div key={index} className="my-10">
+                    <label className="block text-sm font-medium text-gray-900 mb-2">
+                      {field.label}
+                    </label>
+                    <input
+                      className="w-full p-3 border border-gray-300 rounded-md focus:ring-2 focus:ring-blue-500"
+                      value={field.value ?? ""}
+                      onChange={(e) => field.setter(e.target.value)}
+                      type={field.label.includes("ID") ? "number" : "text"}
+                    />
+                  </div>
+                ))}
+              </div>
             </div>
           )}
         </div>
       </div>
-    </div>
-  );
-
-  return (
-    <div className="grid grid-cols-2 gap-4">
-      <div className="flex">{list}</div>
-      <div className="flex">{content}</div>
     </div>
   );
 };
