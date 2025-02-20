a95cdda2c90fa4c9de5b6ec3722cd1f130be0a18
brycecoon <112597142+brycecoon@users.noreply.github.com>
2025-02-20 09:55:55 -0700

    took option away

--- src/Components/InvoiceErrorBanner.tsx
+++ src/Components/InvoiceErrorBanner.tsx
@@ -8,22 +8,22 @@ const InvoiceErrorBanner: React.FC<InvoiceErrorBannerProps> = ({
   return (
     <div className="flex items-center max-md:flex-col gap-6 bg-slate-700 text-white px-6 py-3.5 rounded font-[sans-serif]">
       <p className="text-base flex-1 max-md:text-center">
-        You are creating an invoice with shifts that have already been invoiced
+        There are shifts in this time period that have already been invoiced
       </p>
       <div>
-        <button
+        {/* <button
           type="button"
           className="bg-white text-blue-500 py-2.5 px-5 rounded text-sm"
           onClick={() => generateInvoice(true)}
         >
           Include these shifts
-        </button>
+        </button> */}
         <button
           type="button"
           className="bg-white text-blue-500 py-2.5 px-5 m-5 rounded text-sm"
           onClick={() => generateInvoice(false)}
         >
-          Do not include already invoiced shifts
+          I understand
         </button>
       </div>
     </div>
