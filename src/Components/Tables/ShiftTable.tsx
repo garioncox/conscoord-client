6a77e5851a646d0c394dcd1ccb3260ad2dcd6ca0
Garion Cox <garion.cox@students.snow.edu>
2025-04-03 10:46:30 -0600

    [KGB-405] AvailableShifts - Clicking rapidly to claim a shift breaks the page

--- src/Components/Tables/ShiftTable.tsx
+++ src/Components/Tables/ShiftTable.tsx
@@ -21,12 +21,19 @@ export const ShiftTable: FC<{
   const [sortedData, setSortedData] = useState<Shift[]>(data);
   const { shiftFractionString, shiftFractionStyles, shiftFraction } =
     useShiftsFulfilledUtils();
+  const [isClaimingShift, setIsClaimingShift] = useState<boolean>(false);
 
   useEffect(() => {
     if (data) {
       setSortedData(data);
     }
   }, [data]);
+
+  useEffect(() => {
+    if (!addMutation.isPending) {
+      setIsClaimingShift(false);
+    }
+  }, [addMutation.isPending]);
 
   const TakeShift = (shiftId: number) => {
     addMutation.mutate(shiftId);
@@ -52,92 +59,100 @@ export const ShiftTable: FC<{
         </div>
 
         <div className="overflow-y-auto max-h-[400px] xl:h-[50vh] xl:max-h-full">
-          {sortedData
-            .map((shift) => {
-              const isShiftTaken = userShifts?.some(
-                (userShift) => userShift.id === shift.id
-              );
-              const isShiftFull =
-                shiftFraction(shift) >= 1 || shift.status === "ARCHIVED";
-              const isOverlapping = userShifts?.some(
-                (userShift) =>
-                  userShift.startTime >= shift.startTime &&
-                  userShift.endTime <= shift.endTime
-              );
+          {sortedData.map((shift) => {
+            const isShiftTaken = userShifts?.some(
+              (userShift) => userShift.id === shift.id
+            );
+            const isShiftFull =
+              shiftFraction(shift) >= 1 || shift.status === "ARCHIVED";
+            const isOverlapping = userShifts?.some(
+              (userShift) =>
+                userShift.startTime >= shift.startTime &&
+                userShift.endTime <= shift.endTime
+            );
 
-              const buttonClass =
-                isShiftFull || isOverlapping
-                  ? "text-slate-300 border-slate-300 hover:bg-slate-100 cursor-not-allowed"
-                  : "text-slate-500 border-slate-500 hover:text-white hover:bg-blue-500 hover:border-blue-500";
+            const buttonClass =
+              isShiftFull || isOverlapping
+                ? "text-slate-300 border-slate-300 hover:bg-slate-100 cursor-not-allowed"
+                : "text-slate-500 border-slate-500 hover:text-white hover:bg-blue-500 hover:border-blue-500";
 
-              const buttonTitle =
-                shift.status === "ARCHIVED"
-                  ? "This shift has been archived"
-                  : isShiftFull
-                  ? "Shift is full"
-                  : isOverlapping
-                  ? "Overlapping shift"
-                  : "Take this shift";
+            const buttonTitle =
+              shift.status === "ARCHIVED"
+                ? "This shift has been archived"
+                : isShiftFull
+                ? "Shift is full"
+                : isOverlapping
+                ? "Overlapping shift"
+                : "Take this shift";
 
-              const handleTakeShift = (
-                e: React.MouseEvent<HTMLButtonElement>
-              ) => {
-                e.stopPropagation();
-                if (
-                  !isShiftFull &&
-                  shift.status !== "ARCHIVED" &&
-                  !isOverlapping
-                ) {
-                  TakeShift(shift.id);
-                }
-              };
+            const handleTakeShift = () => {
+              setIsClaimingShift(true);
+              if (
+                !isShiftFull &&
+                shift.status !== "ARCHIVED" &&
+                !isOverlapping
+              ) {
+                TakeShift(shift.id);
+              } else {
+                setIsClaimingShift(false);
+              }
+            };
 
-              return (
-                <div
-                  key={shift.id}
-                  className="grid grid-cols-11 gap-10 py-5 px-3 hover:bg-slate-200 hover:cursor-pointer"
-                  onClick={() => setRowClicked(shift.id)}
-                >
-                  <div className="col-span-3">{shift.location}</div>
-                  <div className="col-span-2">
-                    {CombineTime(shift.startTime, shift.endTime)}
-                  </div>
-                  <div className="col-span-3">{shift.description}</div>
-                  <div className="col-span-2">
-                    <p
-                      className={`flex justify-end ${shiftFractionStyles(
-                        shift
-                      )}`}
-                    >
-                      {shiftFractionString(shift)}
-                    </p>
-                  </div>
-                  <PermissionComponentLock roles={[PSO_ROLE]}>
-                    <div className="flex justify-end pe-2 col-span-1">
-                      {isShiftTaken ? (
-                        <button
-                          onClick={(e) => e.stopPropagation()}
-                          className="bg-emerald-400 group rounded-full cursor-default hover:bg-emerald-400 h-10 w-10 flex items-center justify-center"
-                        >
-                          <Check
-                            className="h-4 w-4 text-white"
-                            strokeWidth={5}
-                          />
-                        </button>
-                      ) : (
-                        <button
-                          className={`rounded-xl bg-tertiary ${buttonClass} border-2 h-10 w-10 flex items-center justify-center`}
-                          title={buttonTitle}
-                          onClick={handleTakeShift}
-                        >
-                          <Plus className="h-4 w-4" strokeWidth={3} />
-                        </button>
-                      )}
-                    </div>
-                  </PermissionComponentLock>
+            let debounceTimeout: ReturnType<typeof setTimeout>;
+
+            const handleDebouncedTakeShift = (
+              e: React.MouseEvent<HTMLButtonElement>
+            ) => {
+              e.stopPropagation();
+              if (debounceTimeout) clearTimeout(debounceTimeout);
+
+              debounceTimeout = setTimeout(() => {
+                handleTakeShift();
+              }, 150);
+            };
+
+            return (
+              <div
+                key={shift.id}
+                className="grid grid-cols-11 gap-10 py-5 px-3 hover:bg-slate-200 hover:cursor-pointer"
+                onClick={() => setRowClicked(shift.id)}
+              >
+                <div className="col-span-3">{shift.location}</div>
+                <div className="col-span-2">
+                  {CombineTime(shift.startTime, shift.endTime)}
                 </div>
-              );
-            })}
+                <div className="col-span-3">{shift.description}</div>
+                <div className="col-span-2">
+                  <p
+                    className={`flex justify-end ${shiftFractionStyles(shift)}`}
+                  >
+                    {shiftFractionString(shift)}
+                  </p>
+                </div>
+                <PermissionComponentLock roles={[PSO_ROLE]}>
+                  <div className="flex justify-end pe-2 col-span-1">
+                    {isShiftTaken ? (
+                      <button
+                        onClick={(e) => e.stopPropagation()}
+                        className="bg-emerald-400 group rounded-full cursor-default hover:bg-emerald-400 h-10 w-10 flex items-center justify-center"
+                      >
+                        <Check className="h-4 w-4 text-white" strokeWidth={5} />
+                      </button>
+                    ) : (
+                      <button
+                        className={`rounded-xl bg-tertiary ${buttonClass} border-2 h-10 w-10 flex items-center justify-center`}
+                        title={buttonTitle}
+                        onClick={handleDebouncedTakeShift}
+                        disabled={addMutation.isPending || isClaimingShift}
+                      >
+                        <Plus className="h-4 w-4" strokeWidth={3} />
+                      </button>
+                    )}
+                  </div>
+                </PermissionComponentLock>
+              </div>
+            );
+          })}
         </div>
       </div>
     </>
